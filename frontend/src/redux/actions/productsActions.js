c3a80f6a13ccd7c294845f022c93836d48b197f5
unknown <lucre.gillone@gmail.com>
2021-10-05 21:39:25 -0300

    Product Form

--- frontend/src/redux/actions/productsActions.js
+++ frontend/src/redux/actions/productsActions.js
@@ -1,5 +1,21 @@
-import axios from "axios";
+import axios from "axios"
 
-const productsActions = {};
+const productsActions = {
+    addProduct: (newProduct) => {
+        return async (dispatch, getState) => {
+            try {
+                let response = await axios.post("http://localhost:4000/api/product/add", {...newProduct})
+                if (response.data.success){
+                    dispatch({type: "ADD_PRODUCT", payload: response.data.respose})
+                }
+                return response
+            } catch(error) {
+                return {
+                    success: false, response: error
+                }
+            }
+        }
+    }
+}
 
-export default productsActions;
+export default productsActions
