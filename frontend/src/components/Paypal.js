e4dc9db83db3ec24122152bdd6143099963b21f7
Darío Rojas <dariourojas07@gmail.com>
2021-10-10 15:30:44 -0300

    paypal button

--- frontend/src/components/Paypal.js
+++ frontend/src/components/Paypal.js
@@ -1,37 +1,36 @@
 import { useEffect, useRef } from "react"
 
 const Paypal = (props) => {
-  console.log(props)
   const paypal = useRef()
   useEffect(() => {
-    window.paypal
-      .Buttons({
-        createOrder: (data, actions, err) => {
-          return actions.order.create({
-            intent: "CAPTURE",
-            purchase_units: [
-              {
-                description: props.description,
-                amount: {
-                  value: props.total,
-                  currency_code: "USD",
-                },
+    if (window.myButton) window.myButton.close()
+    window.myButton = window.paypal.Buttons({
+      createOrder: (data, actions, err) => {
+        return actions.order.create({
+          intent: "CAPTURE",
+          purchase_units: [
+            {
+              description: props.description,
+              amount: {
+                value: props.total,
+                currency_code: "USD",
               },
-            ],
-          })
-        },
-        onApprove: async (data, actions) => {
-          await actions.order.capture()
-          console.log("entro al on aprove")
+            },
+          ],
+        })
+      },
+      onApprove: async (data, actions) => {
+        await actions.order.capture()
+        console.log("entro al on aprove")
 
-          //   props.history.push("/")
-        },
-        onError: (err) => {
-          alert("Hubo un error!")
-          console.log(err)
-        },
-      })
-      .render(paypal.current)
+        //   props.history.push("/")
+      },
+      onError: (err) => {
+        alert("Hubo un error!")
+        console.log(err)
+      },
+    })
+    window.myButton.render(paypal.current)
   })
 
   return <div ref={paypal}></div>
