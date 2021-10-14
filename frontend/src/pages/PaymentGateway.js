2eb015f1be5072362bbf2b8c7c0e0ca5fc2e99cd
Carla Alderete <carlamalderete@gmail.com>
2021-10-14 12:17:50 -0300

    14-10-12:17

--- frontend/src/pages/PaymentGateway.js
+++ frontend/src/pages/PaymentGateway.js
@@ -483,6 +483,9 @@ const PaymentGateway = ({
           {chosenMethod.enable && chosenMethod.type.includes("MercadoPago") && (
             <PayWithCard addNewOrderHandler={addNewOrderHandler} total={!sharedPayment ? order.totalPrice : sharedPaymentPrice} catchPagoErr={catchPagoErr}/>
           )}
+          {/* {chosenMethod.enable && chosenMethod.type.includes("MercadoPago") && (
+            <MercadoPagoForm addNewOrderHandler={addNewOrderHandler} total={!sharedPayment ? order.totalPrice : sharedPaymentPrice} catchPagoErr={catchPagoErr}/>
+          )} */}
         </div>
         <SideProducts products={products} total={order.totalPrice} />
       </div>
